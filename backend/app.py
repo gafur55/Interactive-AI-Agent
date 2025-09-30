6a72cc75207f8b6a2682562e35d5301d06fd3d7c
Gafur Mammadov <101157079+gafur55@users.noreply.github.com>
2025-09-30 01:26:59 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -51,7 +51,6 @@ openai.api_key = OPENAI_API_KEY
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
